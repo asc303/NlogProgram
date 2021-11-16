c1939d6ed66247ad14014e5ac4abc3add7806a6b
asc303 <chauhansam94@gmail.com>
2021-11-16 13:35:23 +0530

    [ashutosh] add:NlogProgram

---
+++ NlogProgram/NlogProgram/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace NlogProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AddNumbers add = new AddNumbers();
+            add.Sum(34, 45);
+
+        }
+    }
+}
